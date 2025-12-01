98fcf7b0f09caf2c9a1bfeec481df10b2b2c7f16
Alexander Breadman <alexander.breadman@chase.io>
2025-12-01 20:33:17 +0000

    auto

--- pkg/road/definitions.go
+++ pkg/road/definitions.go
@@ -12,10 +12,11 @@ const (
 	ServiceTypeCamping
 )
 
+// this should be the new contents of files in assets/level/*.level
 type LevelDefinition struct {
-	Laybys []*Layby
+	Laybys []*Layby `json:"laybys"`
 	// keep the lane definitions eg. "AAA" (without the X as laybys handles this)
-	Segments []string
+	Segments []string `json:"segments"`
 }
 
 // use layby to generate the length of a layby from the services it contains. each service should be at least 1 segment long.
